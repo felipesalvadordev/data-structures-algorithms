f54c2042364c69434de6a229b1a468434dce614f
Felipe Salvador <f.salvador91@gmail.com>
2025-04-05 17:46:16 -0300

    maximum depth of binary tree using breadth first search with queue impl

---
+++ Data-Structures-Algorithms/Data-Structures/Queues/MaximumDepthOfBinaryTree.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data_Structures_Algorithms.Data_Structures.Queues
+{
+    internal class MaximumDepthOfBinaryTree
+    {
+        public void Test()
+        {
+            /*
+            *     1
+            *    / \
+            *   2   3
+            *      / 
+            *     4           
+            */
+
+            TreeNode root = new TreeNode(1)
+            {
+                left = new TreeNode(2),
+                right = new TreeNode(3) { left = new TreeNode(4)}
+            };
+
+            var maxDepth = MaxDepth(root);
+            Console.WriteLine(maxDepth);
+        }
+
+        //Breadth First Search
+        public int MaxDepth(TreeNode root)
+        {
+            Queue<TreeNode> q = new Queue<TreeNode>();
+            if (root != null)
+            {
+                q.Enqueue(root);
+            }
+
+            int level = 0;
+            while (q.Count > 0)
+            {
+                int size = q.Count;
+                for (int i = 0; i < size; i++)
+                {
+                    TreeNode node = q.Dequeue();
+                    if (node.left != null)
+                    {
+                        q.Enqueue(node.left);
+                    }
+                    if (node.right != null)
+                    {
+                        q.Enqueue(node.right);
+                    }
+                }
+                level++;
+            }
+            return level;
+        }
+    }
+}
