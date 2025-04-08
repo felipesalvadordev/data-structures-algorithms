6c4cba27ccebe0dc76ab5f87037d69d8bb6448c8
Felipe Salvador <f.salvador91@gmail.com>
2025-04-08 20:50:12 -0300

    prefix sum impl

---
+++ Data-Structures-Algorithms/ProblemSolvingPatterns/PrefixSum/LargestAltitude.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Structures_Algorithms.ProblemSolvingPatterns.PrefixSum
+{
+    internal class LargestAltitude
+    {
+        public void Test()
+        {
+            int[] gain = new int[] { -5, 1, 5, 0, -7 };
+            Console.WriteLine(LargestAltitudeImpl(gain));
+        }
+
+        private int LargestAltitudeImpl(int[] gain)
+        {
+
+            var highestAltitude = 0;
+            var lastHeight = 0;
+
+            for (var i = 0; i < gain.Length; i++)
+            {
+                var currentHeight = gain[i];
+                var currentGain = currentHeight + lastHeight;
+                highestAltitude = Math.Max(highestAltitude, currentGain);
+                lastHeight = currentGain;
+            }
+            return highestAltitude;
+        }
+    }
+}
