1f322284297d8dcda0d36fd8a5b3135075a56e38
Felipe Salvador <f.salvador91@gmail.com>
2025-05-31 15:17:21 -0300

    monotonic stack impl

--- Data-Structures-Algorithms/Data-Structures/Stacks/MonotonicStack.cs
+++ Data-Structures-Algorithms/Data-Structures/Stacks/MonotonicStack.cs
@@ -1,50 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Data_Structures_Algorithms.Data_Structures.Stacks
 {
-    internal class LargestRectangleInArea
+    //Using Single Stack - O(n) Time and O(n) Space 
+    internal class MonotonicStack
     {
-        public void Test()
-        {
-            //Integer[] areas = {11, 11, 10, 10, 10};//50
-            //Integer[] area = {1, 2, 3, 4, 5};//9
-            int[] areas = { 2, 1, 5, 6, 2, 3 };//10;
-            var maxArea = LargestRectangleArea(areas);
-            Console.Write("Max rectangle is " + maxArea);
-        }
-
         // 1 - Look at the height for a column.
         // 2 - If the height is greater than or equal to the first height at the top of the stack or if the stack is empty, push the height onto the stack.
         // 3 - Otherwise pop from the stack.  The maximum rectangular area that exists for this popped height is then calculated.
         // 4 - Continue to pop until the top stack height is less than the column height from step 1.
         static int LargestRectangleArea(int[] heights)
         {
-            int n = heights.Length;
-            Stack<int> heightsPositionStack = new Stack<int>();
+            Stack<int> stack = new Stack<int>();
             int maxArea = 0;
+            int n = heights.Length;
+            int[] newHeights = new int[n];
+            Array.Copy(heights, newHeights, n); // Add sentinel 0 at the end
 
-            // Traverse all bars of the histogram
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i <= n; i++)
             {
                 // Process the stack while the current element 
                 // is smaller than the element corresponding to 
                 // the top of the stack
-                while (heightsPositionStack.Count > 0 && heights[heightsPositionStack.Peek()] >= heights[i])
+                while (stack.Count > 0 && (i == n || newHeights[i] < newHeights[stack.Peek()]))
                 {
-                    var topHeight = heightsPositionStack.Pop();
                     //Minus 1 to the get index of the current top height in stack
                     //Index i is the width we came across so far (this width does not include the element
                     //that we are iterating now)
                     //The width is determined by the difference between the current index and the new top of the stack.
-                    int width = heightsPositionStack.Count == 0 ? i : i - heightsPositionStack.Peek() - 1;
-                    int lastMaxAreaCalculated = heights[topHeight] * width;
-                    maxArea = Math.Max(maxArea, lastMaxAreaCalculated);
+                    int maxHeight = newHeights[stack.Pop()];
+                    int width = stack.Count == 0 ? i : i - stack.Peek() - 1;
+                    maxArea = Math.Max(maxArea, maxHeight * width);
                 }
-                heightsPositionStack.Push(i);
+                stack.Push(i);
             }
+
             return maxArea;
+        }
+
+        public void Test()
+        {
+            int[] areas = { 2, 1, 5, 6, 2, 3 };//10
+            //int[] areas = { 1, 2, 3, 4, 5 };//9
+            //int[] areas = { 11, 11, 10, 10, 10 };//50
+            Console.WriteLine($"Largest rectange in histogram bars is: {LargestRectangleArea(areas)}"); 
         }
     }
 }
